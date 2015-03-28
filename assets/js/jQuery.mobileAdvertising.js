30dad00f447d8039fbb335ec7195d3311807cda6
Roostam <chirurg@inbox.ru>
2015-03-28 18:54:41 +0300

    Вывод рекламы по стране если указан с предварительной дата фильтрацией

--- assets/js/jQuery.mobileAdvertising.js
+++ assets/js/jQuery.mobileAdvertising.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Mobile advertising: v0.1.0
+ * Mobile advertising: v0.1.2
  *
  * ========================================================================
  * Copyright 2015 Alimov.
@@ -11,10 +11,8 @@ if (typeof jQuery == 'undefined') {
 }
 
 +(function($){
-    'use strict';
-
     // Mobile advertising CLASS DEFINITION
-    // ====================
+    // ===================================
 
     var Mob = function (element, options) {
 
@@ -28,52 +26,50 @@ if (typeof jQuery == 'undefined') {
         }));
     };
 
-    Mob.VERSION = '0.1.0';
+    Mob.VERSION = '0.1.2';
 
     Mob.DEFAULTS = {
-        'json_folder'   : "/assets/advertising/"
+        'json_folder'   : "/assets/advertising/",
+        'tpl_folder'    : "assets/templates/default/"
     };
 
     // Mobile show modal
-    // =====================
+    // =================
 
     Mob.prototype.show  = function (e) {
 
-        var content1 = '<div class="modal-dialog modal-sm">'+
-            '<div class="modal-content">'+
-            '<div class="modal-header color-white clearfix">'+
-
-            '<div class="col-xs-8 not-padding-w">'+
-            '<h4 class="modal-title color-white">'+ e.content.title +'</h4>'+
-            '</div>'+
-
-            '<div class="col-xs-4 not-padding-w">'+
-            '<div class="clearfix mb5px">'+
-            '<button type="button" class="close color-white" data-dismiss="modal" aria-label="Close"><span class="color-white" aria-hidden="true">&times;</span></button>'+
-            '</div>'+
-            '<a href="' + e.content.url + '" class="btn text-uppercase btn-success btn-xs notbdrs pull-right">Бесплатно</a>'+
-            '</div>'+
-
-            '</div>'+
-
-            '<div class="modal-body">'+
-            e.content.content +
-            '</div>'+
-
-            '<div class="modal-footer text-center">'+
-            '<div class="clearfix mb5px">'+
-            '<a href="'+ e.content.url +'" class="btn btn-'+e.content.colorButton+' text-uppercase btn-block"><i class="glyphicon glyphicon-download-alt"></i> '+ e.content.textButton +'</a>'+
-            '</div>'+
-            '</div>'+
-            '</div>'+
-            '</div>';
-
-        e.$element.append(content1).fadeIn('slow');
+        $.ajax({
+            url: e.options["tpl_folder"] + e.suffix +'.tpl',
+            dataFilter: function(str){ //http://javascript.ru/unsorted/templating
+                function Tpl (str, data){
+                    var fn = !/\W/.test(str) ? cache[str] = cache[str] :
+                        new Function("obj",
+                            "var p=[],print=function(){p.push.apply(p,arguments);};with(obj){p.push('" +
+                            str
+                                .replace(/[\r\t\n]/g, " ")
+                                .split("{{").join("\t")
+                                .replace(/((^|}})[^\t]*)'/g, "$1\r")
+                                .replace(/\t=(.*?)}}/g, "',$1,'")
+                                .split("\t").join("');")
+                                .split("}}").join("p.push('")
+                                .split("\r").join("\\'")
+                            + "');}return p.join('');");
+                    return data ? fn(data) : fn;
+                }
+                return e.$element.innerHTML = Tpl(str, e.content);
+            },
+            success: function (data) {
+               $(data).appendTo(e.$element).fadeIn();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR, textStatus, errorThrown);
+            }
+        });
 
     };
 
     // Mobile advertising claim ajax to json
-    // =====================
+    // =====================================
 
     Mob.prototype.claim = function (e, callback) {
         var keyAdvertising  = ("suffix" in e) ? e.options[e.suffix] : false;
@@ -83,6 +79,7 @@ if (typeof jQuery == 'undefined') {
             dataType: 'json',
             scriptCharset: 'utf-8',
             timeout: 10000,
+            contentType: 'application/json',
             dataFilter: function(data, type){
                 if ((type == "json") && ("geoLocation" in e)) {
                     var parsed_data = JSON.parse(data);
@@ -96,6 +93,7 @@ if (typeof jQuery == 'undefined') {
                     });
                     return JSON.stringify(parsed_data)
                 }
+                else return data;
             },
             success: function (data) {
                 var randomKey = Math.round(0.5 + Math.random() * (Object.keys(data).length)),
@@ -117,7 +115,7 @@ if (typeof jQuery == 'undefined') {
     };
 
     // Mobile advertising PLUGIN DEFINITION
-    // =====================
+    // ====================================
 
     function Plugin(option) {
         return this.each(function () {
@@ -144,7 +142,7 @@ if (typeof jQuery == 'undefined') {
     };
 
     // Mobile advertising check devise
-    // ===============
+    // ===============================
 
     function isMobile(){ // Проверка браузера мобилки (есть возможность дописать больше или разграничить технику apple)
         var Device = {
@@ -170,8 +168,8 @@ if (typeof jQuery == 'undefined') {
         return (Device.Android() || Device.BlackBerry() || Device.iOS() || Device.Opera() || Device.Windows() || Device.Chrome());
     }
 
-    //Mobile advertising DATA-API
-    //=================
+    // Mobile advertising DATA-API
+    // ===========================
 
     $(window).on('load',function(){
         var counter = 0;
